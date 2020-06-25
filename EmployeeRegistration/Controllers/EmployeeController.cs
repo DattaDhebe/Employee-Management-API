bb32fb2d0940c705e7b118256af41d9900edb7c2
DattaDhebe <dattadhebe75@gmail.com>
2020-06-26 01:54:37 +0530

    Added : Added Iconfiguration interface to access appsettings.json file.

--- EmployeeRegistration/Controllers/EmployeeController.cs
+++ EmployeeRegistration/Controllers/EmployeeController.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using EmployeeRegistration;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 
 namespace EmployeeRegistration.Controllers
 {
@@ -11,5 +15,38 @@ namespace EmployeeRegistration.Controllers
     [ApiController]
     public class EmployeeController : ControllerBase
     {
+        public IConfiguration Configuration { get; }
+        public EmployeeController(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create_Post(Employees employees)
+        {
+            if (ModelState.IsValid)
+            {
+                string connectionString = Configuration["ConnectionStrings:DefaultConnection"];
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    string sql = $"Insert Into Employee (EmployeeName, Salary) Values ('{employees.EmployeeName}','{employees.Salary}')";
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        command.CommandType = CommandType.Text;
+                        connection.Open();
+                        command.ExecuteNonQuery();
+                        connection.Close();
+                    }
+                    return RedirectToAction("Index");
+                }
+            }
+            else
+                return View();
+        }
     }
 }
