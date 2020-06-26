d952dcbb8d5b7a6325a0858c2ff340728e966b23
DattaDhebe <dattadhebe75@gmail.com>
2020-06-26 23:44:44 +0530

    Refactored : Change in Database Connection and CommanLayer EmployeeData.

---
+++ CommanLayer/Employees.cs
@@ -0,0 +1,37 @@
+﻿using EmployeeRegistration;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeRegistration
+{
+	public class Employees
+	{
+		public Employees()
+		{
+		}
+
+		[Required]
+		public int Id { get; set; }
+
+		[Required]
+		public string FirstName { get; set; }
+
+		[Required]
+		public string LastName { get; set; }
+
+		[Required]
+		public string ContactNumber { get; set; }
+
+		[Required]
+		public string City { get; set; }
+
+		[Required]
+		public decimal Salary { get; set; }
+
+		[Required]
+		public DateTime JoiningDate { get; set; }
+	}
+}
