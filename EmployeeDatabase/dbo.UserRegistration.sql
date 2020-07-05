84fbab0b0430fe3d8ce9a13712357a4e3447e2fe
DattaDhebe <dattadhebe75@gmail.com>
2020-07-05 19:07:03 +0530

    Added : Create Table File for Creation of Table for the Problem.

---
+++ EmployeeDatabase/dbo.UserRegistration.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[UserRegistration] (
+    [Id]           INT          IDENTITY (1, 1) NOT NULL,
+    [UserName] VARCHAR (50) NULL,
+    [Username]     VARCHAR (50) NOT NULL,
+    [Email]        VARCHAR (50) UNIQUE NOT NULL,
+    [Password]     VARCHAR (50) NOT NULL,
+    [Gender]       VARCHAR (50) NULL,
+    [City]         VARCHAR (50) NULL,
+    [CreatedDate]  VARCHAR (50) NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
